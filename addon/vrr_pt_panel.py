1652e91d98d361f3d59007b0b4a04c9d291bff7a
pmzeroskyline <guzhang0@gmail.com>
2023-06-20 16:01:28 +0800

    panel label

--- addon/vrr_pt_panel.py
+++ addon/vrr_pt_panel.py
@@ -2,13 +2,13 @@ import bpy
 
 class VRR_PT_Panel(bpy.types.Panel):
     bl_idname = "OBJECT_PT_air_panel"
-    bl_label = "Air Panel"
+    bl_label = "VRoid Rig"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_category = "VRig"
+    bl_category = "VRoidRig"
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Welcome VRoidRig Addon")
+        layout.label(text="Welcome !")
         layout.operator("vrr.rig")
 
